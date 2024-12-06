68bc3c9d2ffd618f4e835948f50595986480e75e
DhrumitPanchal <dhrumitpanchal789@gmail.com>
2024-12-06 21:35:33 +0530

    changed apk download method

--- src/app/components/DownloadAPK.tsx
+++ src/app/components/DownloadAPK.tsx
@@ -1,39 +1,14 @@
-"use client";
-
-import React from "react";
-import axios from "axios";
-
 function DownloadAPK() {
-  const handleDownload = async () => {
-    try {
-      const response = await axios.get(
-        "https://prostock-website.vercel.app/api",
-        {
-          responseType: "blob", // Important to handle file downloads
-        }
-      );
-
-      // Create a blob from the response and trigger the download
-      const url = window.URL.createObjectURL(new Blob([response.data]));
-      const link = document.createElement("a");
-      link.href = url;
-      link.setAttribute("download", "prostock.apk"); // Specify the file name
-      document.body.appendChild(link);
-      link.click();
-      document.body.removeChild(link);
-    } catch (error) {
-      console.error("Error downloading APK:", error);
-      alert("An error occurred while downloading the APK. Please try again.");
-    }
-  };
-
   return (
-    <button
-      onClick={handleDownload}
-      className="single-element flex items-center justify-center mt-6 h-12 w-44 font-bold uppercase text-white bg-theme_color rounded-md"
-    >
-      Download now
-    </button>
+    <>
+      <a
+        href="/prostock.apk" // URL to the file in the public folder
+        download="prostock.apk" // Suggests the filename to the browser
+        className="single-element flex items-center justify-center mt-6 h-12 w-44 font-bold uppercase text-white bg-theme_color rounded-md"
+      >
+        Download now
+      </a>
+    </>
   );
 }
 
