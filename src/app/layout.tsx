46c6d029e9153be54704c32cdd8b800550bd04a8
DhrumitPanchal <dhrumitpanchal789@gmail.com>
2025-04-17 23:53:56 +0530

    chnage in meta tages

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -39,7 +39,7 @@ n.queue=[];t=b.createElement(e);t.async=!0;
 t.src=v;s=b.getElementsByTagName(e)[0];
 s.parentNode.insertBefore(t,s)}(window, document,'script',
 'https://connect.facebook.net/en_US/fbevents.js');
-fbq('init', '569375935598283');
+fbq('init', '994328532425019');
 fbq('track', 'PageView');
 `,
           }}
@@ -47,7 +47,7 @@ fbq('track', 'PageView');
         <noscript
           dangerouslySetInnerHTML={{
             __html: `<img height="1" width="1" style="display:none"
-src="https://www.facebook.com/tr?id=569375935598283&ev=PageView&noscript=1"
+src="https://www.facebook.com/tr?id=994328532425019&ev=PageView&noscript=1"
 />`,
           }}
         ></noscript>
